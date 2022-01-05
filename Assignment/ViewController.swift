d50ab5152842ddfbd3d5802de7238c2ce201b8ea
Ben Shek <benshek49@gmail.com>
2022-01-05 22:44:18 +0800

    Basic map view and camera constraints

--- Assignment/ViewController.swift
+++ Assignment/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let map = MKMapView()
+    let hkCoord = CLLocation(latitude: 22.3193, longitude: 114.1694)
+    let mapBoundary = MKCoordinateRegion()
+    
+    @IBOutlet private var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.centerToLocation(hkCoord)
+        
+        // Set the region for camera
+        let region = MKCoordinateRegion(center: hkCoord.coordinate,
+                                        latitudinalMeters: 55000,
+                                        longitudinalMeters: 70000)
+        
+        // Set camera zoom out range
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 100000)
+        
+        // Set camera constrains
+        mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region),
+                                  animated: true)
+        mapView.setCameraZoomRange(zoomRange, animated: true)
     }
+    
+    
+}
 
-
+// Center current view to desired region
+private extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 1000) {
+        let coordRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordRegion, animated: true)
+    }
 }
 
